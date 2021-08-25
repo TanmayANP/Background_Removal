018ba0793e1e6ab7eeb84c22e74606525022c0a2
Tammy_P <tanmay.puppalwar26@gmail.com>
2021-08-25 15:43:11 +0530

    Update remove_bg.py

--- remove_bg.py
+++ remove_bg.py
@@ -9,7 +9,7 @@ def remove_background(input_path):
     img_file = Image.open(input_path)
     img_file.save('inputs\modified_img.jpg')
     change_bg = alter_bg()
-    change_bg.load_pascalvoc_model('model\deeplabv3_xception_tf_dim_ordering_tf_kernels.h5')
+    change_bg.load_pascalvoc_model('deeplabv3_xception_tf_dim_ordering_tf_kernels.h5')
     output_path = 'outputs\only_object.jpg'
     change_bg.color_bg('inputs\modified_img.jpg', colors=(255, 255, 255), output_image_name=output_path)
     return output_path
